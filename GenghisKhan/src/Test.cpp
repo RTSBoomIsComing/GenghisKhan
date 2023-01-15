768cb63eecc29a9e7b7a1fc8354b063a80b18ae9
yudaegam <61501369+yudaegam@users.noreply.github.com>
2023-01-15 23:48:47 +0900

    first commit

---
+++ GenghisKhan/src/Test.cpp
@@ -0,0 +1,6 @@
+#include "Test.h"
+#include <iostream>
+void Khan::Print()
+{
+	std::cout << "Welcome to Genghis Khan World!" << std::endl;
+}
---
+++ GenghisKhan/src/Test.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace Khan
+{
+	__declspec(dllexport) void Print();
+}
