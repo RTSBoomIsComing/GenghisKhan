2c7db252e62d6d1ec2e1a03a386f299ac516ee89
yudaegam <61501369+yudaegam@users.noreply.github.com>
2023-03-11 23:33:49 +0900

    Remove Systems.h

--- Sandbox/Shader/PS_Laser.hlsl
+++ Sandbox/Shader/PS_Laser.hlsl
@@ -1,4 +1,66 @@
-float4 main() : SV_TARGET
+cbuffer ConstantsOthers : register(b0)
 {
-	return float4(1.0f, 1.0f, 1.0f, 1.0f);
+	matrix InverseViewProjection; // hlsl does not provide function to invert a matrix
+}
+
+struct PS_IN
+{
+	float4 pos : SV_POSITION;
+	float2 uv : TEXCOORD;
+};
+
+struct PS_OUT
+{
+	float4 color : SV_TARGET;
+	float depth : SV_DEPTH;
+};
+
+PS_OUT main(PS_IN input)
+{
+	PS_OUT output = (PS_OUT)0;
+	// note : this uv range is (-1, 1), not (0, 1)
+	// this uv is not used for texcoord 
+	float4 near = float4(input.uv, 0.0F, 1.0F);		// (x, y, 0.0, 1.0)
+	float4 far = float4(input.uv, 1.0F, 1.0F);	// (x, y, 1.0, 1.0)
+
+	// calculate near(start) point of ray 
+	near = mul(near, InverseViewProjection);
+	near = near / near.w;
+
+	// calculate far(end) point of ray
+	far = mul(far, InverseViewProjection);
+	far = far / far.w;
+
+	// equation, y = near.y + t * (far.y - near.y)
+	// t is weight for interpolation
+	// if y is 0, t = -near.y / (far.y - near.y)
+	// the condition (y is 0) means that a point interpolated by t would be on a floor
+
+	float t = -near.y / (far.y - near.y);
+	float4 intersection = near + t * (far - near);
+
+	float scale = 0.01F; // grid interval is 100.0F
+	float2 floorPos = intersection.xz * scale;
+	float2 derivative = fwidth(floorPos);
+
+	clip(1.0F - t);	// Clip if a intersection point is farther than far point.
+	clip(t);		// Clip if there is no intersection.
+	clip(2.0F - derivative);	// If a intersection point is far away, it will be drawn very small.
+	// So clip it.
+
+// talk about frac(x - 0.5F) - 0.5F,
+// frac function calculate x - floor(x),
+// if x < 0, e.g x = -1.4F, floor(-1.4F) = -2.0F, so frac(-1.4F) = -1.4F - (-2.0F) = 0.6F
+// that is not we expected, we want -0.4F for frac(-1.4F)
+// then frac(x - 0.5F) - 0.5F do what we want
+// if x = -1.4F, frac(-1.4F - 0.5F) = frac(-1.9F) = -1.9F - (-2.0F) = 0.1F
+// frac(-1.4F - 0.5F) - 0.5F = 0.1F - 0.5F = -0.4F, that is exactly we expected
+	float2 grid = abs(frac(floorPos - 0.5F) - 0.5F) / (derivative * 2); // keep thickness of grid line uniformly on screen
+	float gridLine = min(grid.x, grid.y);
+	output.color.xyz = float3(1.0F, 1.0F, 1.0F) * pow(1.0F - t, 4); // anti-aliasing
+	output.color.a = (1.0 - saturate(gridLine)) * pow(1.0F - t, 4); // anti-aliasing
+
+	float rayZ = length(intersection - near);
+	output.depth = rayZ / (rayZ + 1.0F);
+	return output;
 }
